6e1b1835ec5f7b2c36313710dd36d9ffcfb02f58
bhush-r <bhushanlilhare956@gmail.com>
2024-08-02 20:01:50 +0530

    Complete the Assigment

---
+++ app/src/main/java/com/example/openinapp/api/OpeninApi.kt
@@ -0,0 +1,13 @@
+package com.example.openinapp.api
+
+import com.example.openinapp.data.model.DashboardResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface OpeninApi {
+
+    @GET("api/v1/dashboardNew")
+//    @GET("https://api.inopenapp.com/api/v1/dashboardNew")
+    suspend fun dashboardApi(): Response<DashboardResponse>
+}
